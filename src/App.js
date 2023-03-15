31254a8ed9721e4b9b9bed752f31493c6debcb97
Luis Diaz <luisedgucv@gmail.com>
2023-03-15 11:52:03 -0500

    added PhotoCard, installed react icons and added a list for the photoCards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { ListOfCategories } from './components/ListOfCategories';
 import { GlobalStyle } from './GlobalStyles';
+import { PhotoCard } from './components/PhotoCard';
+import { ListOfPhotoCards } from './components/ListOfPhotoCards';
 
 export const App = () => (
     <div>
         <ListOfCategories />
         <GlobalStyle />
+        <ListOfPhotoCards />
     </div>
-    
 )
