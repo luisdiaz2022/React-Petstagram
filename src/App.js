8d799e1d49ab20eee65b8bb2b6f370b9022eeb8a
luis diaz <Luis>
2023-03-19 19:14:47 -0500

    added animations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ListOfCategories } from './components/ListOfCategories';
-import { GlobalStyle } from './GlobalStyles';
-import { PhotoCard } from './components/PhotoCard';
+import { GlobalStyle } from './components/styles/GlobalStyles';
 import { ListOfPhotoCards } from './components/ListOfPhotoCards';
 
 export const App = () => (
