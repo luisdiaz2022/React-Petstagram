10f7de70a2a6ef417d26a25dba3aa690735b2ddd
luis diaz <Luis>
2023-03-20 11:01:54 -0500

    added cast on hook, modified styles for listOfCategories, added a loading view

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,17 +2,26 @@ import React, {Fragment, useEffect, useState} from 'react';
 import { Category } from '../Category';
 import { List, Item } from './styles';
 
-export const ListOfCategories = () => {
+function useCategoriesData() {
     const [categories, setCategories] = useState([])
-    const [showFixed, setShowFixed] = useState(false)
+    const [loading, setLoading] = useState(false)
 
     useEffect(function() {
+        setLoading(true)
         window.fetch('https://petgram-server-luisedgucv2-luisdiaz2022.vercel.app/categories')
         .then (res => res.json ())
         .then (response => {
             setCategories(response)
+            setLoading(false)
         })
     }, [])
+
+    return {categories, loading}
+}
+
+export const ListOfCategories = () => {
+    const {categories, loading} = useCategoriesData()
+    const [showFixed, setShowFixed] = useState(false)
 
     useEffect(function() {
 
@@ -28,7 +37,9 @@ export const ListOfCategories = () => {
     const renderList = (fixed) => (
         <List fixed={fixed}>
             {
-            categories.map(category => <Item key= {category.id}><Category {...category} /></Item>)
+                loading
+                    ? <Item key='loading'> <Category /> </Item>
+                : categories.map(category => <Item key= {category.id}><Category {...category} /></Item>)
             }
         </List>
     )
