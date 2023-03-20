6642e0aaeb9e66f1312a5a17ac4036c2321baba0
luis diaz <Luis>
2023-03-20 10:38:49 -0500

    added useEffect for categories, now we have a double category bar, added logo

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { ListOfCategories } from './components/ListOfCategories';
 import { GlobalStyle } from './components/styles/GlobalStyles';
 import { ListOfPhotoCards } from './components/ListOfPhotoCards';
+import { Logo } from './components/Logo';
 
 export const App = () => (
     <div>
-        <ListOfCategories />
         <GlobalStyle />
+        <Logo />
+        <ListOfCategories />
         <ListOfPhotoCards />
     </div>
 )
--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import { Category } from '../Category';
 import { List, Item } from './styles';
 
 export const ListOfCategories = () => {
     const [categories, setCategories] = useState([])
+    const [showFixed, setShowFixed] = useState(false)
 
     useEffect(function() {
         window.fetch('https://petgram-server-luisedgucv2-luisdiaz2022.vercel.app/categories')
@@ -12,12 +13,30 @@ export const ListOfCategories = () => {
             setCategories(response)
         })
     }, [])
-    
-    return (
-        <List>
+
+    useEffect(function() {
+
+        const onScroll = e => {
+            const newShowFixed = window.scrollY > 200
+            showFixed != newShowFixed && setShowFixed(newShowFixed)
+        }
+        document.addEventListener('scroll', onScroll)
+
+        return () => document.removeEventListener('scroll', onScroll)
+    }, [showFixed]) 
+
+    const renderList = (fixed) => (
+        <List fixed={fixed}>
             {
             categories.map(category => <Item key= {category.id}><Category {...category} /></Item>)
             }
         </List>
+    )
+
+    return (
+        <Fragment>
+            {renderList()}
+            {showFixed && renderList(true)}
+        </Fragment>
     )
 }
