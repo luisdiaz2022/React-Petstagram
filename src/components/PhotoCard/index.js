9ebba0ad323b32f9967ce63a8ab590e2a07a9eeb
luis diaz <Luis>
2023-03-20 13:16:34 -0500

    added local storage for likes, also added a new icon

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,27 +1,53 @@
 import React, {Fragment, useEffect, useRef, useState} from 'react';
 import { ImgWrapper, Img, Button, Article } from './styles';
-import { MdFavoriteBorder} from "react-icons/md";
+import { MdFavoriteBorder, MdFavorite} from "react-icons/md";
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
     const element = useRef(null);
     const [show, setShow] = useState(false);
+    const key = `like-${id}`
+    const [liked, setLiked] = useState(() => {
+        try {
+            const like = window.localStorage.getItem(key);
+            return like;
+        } catch (e) {
+            return false;
+        }
+    });
+
+    console.log(liked)
 
     useEffect(function () {
-        const observer = new window.IntersectionObserver(function(entries) {
-            const {isIntersecting} = entries[0]
-            console.log(isIntersecting)
-
-            if (isIntersecting) {
-                setShow(true);
-                observer.disconnect();
-            }
-        })
-
-        observer.observe(element.current);
-
+        Promise.resolve(
+            typeof window.IntersectionObserver != 'undefined'
+                ? window.IntersectionObserver
+                : import('intersection-observer')
+        ).then (() => {
+                const observer = new window.IntersectionObserver(function(entries) {
+                    const {isIntersecting} = entries[0]
+        
+                    if (isIntersecting) {
+                        setShow(true);
+                        observer.disconnect();
+                    }
+                })
+        
+                observer.observe(element.current);
+            })
     },[element])
+
+    const Icon = liked ? MdFavorite : MdFavoriteBorder;
+
+    const setLocalStorage = value => {
+        try {
+            window.localStorage.setItem(key, value);
+            setLiked(value);
+        } catch (e) {
+            console.error(e);
+        }
+    }
 
     return(
         <Article ref={element}>
@@ -33,8 +59,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
                         </ImgWrapper>
                     </a>
 
-                    <Button>
-                        <MdFavoriteBorder size = '32px'/> {likes} likes!
+                    <Button onClick={() => setLocalStorage(!liked)}>
+                        <Icon size = '32px'/> {likes} likes!
                     </Button>
                 </Fragment>
             }
