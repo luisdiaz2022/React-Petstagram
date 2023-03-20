10a9e2ec2d43d7e398b6ad2dffbbf08e6a11ee6c
luis diaz <Luis>
2023-03-20 11:35:16 -0500

    added observer/lazyload to photo cards

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,20 +1,43 @@
-import React from 'react';
-import { ImgWrapper, Img, Button } from './styles';
-import { MdFavoriteBorder } from "react-icons/md";
+import React, {Fragment, useEffect, useRef, useState} from 'react';
+import { ImgWrapper, Img, Button, Article } from './styles';
+import { MdFavoriteBorder} from "react-icons/md";
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
+
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+    const element = useRef(null);
+    const [show, setShow] = useState(false);
+
+    useEffect(function () {
+        const observer = new window.IntersectionObserver(function(entries) {
+            const {isIntersecting} = entries[0]
+            console.log(isIntersecting)
+
+            if (isIntersecting) {
+                setShow(true);
+                observer.disconnect();
+            }
+        })
+
+        observer.observe(element.current);
+
+    },[element])
 
     return(
-        <article>
-            <a href = {`/details/${id}`}>
-                <ImgWrapper>
-                    <Img src = {src} />
-                </ImgWrapper>
-            </a>
-            <Button>
-                <MdFavoriteBorder size = '32px'/> {likes} likes!
-            </Button>
-        </article>
+        <Article ref={element}>
+            {
+                show && <Fragment>
+                    <a href = {`/details/${id}`}>
+                        <ImgWrapper>
+                            <Img src = {src} />
+                        </ImgWrapper>
+                    </a>
+
+                    <Button>
+                        <MdFavoriteBorder size = '32px'/> {likes} likes!
+                    </Button>
+                </Fragment>
+            }
+        </Article>
     )
 }
