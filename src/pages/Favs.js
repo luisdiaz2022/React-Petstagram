0e922fadd3153352e2f88b289167b4d0baa154e3
Luis Diaz <Luis>
2023-03-27 12:58:00 -0500

    added list of fav

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,3 +1,18 @@
 import React from "react";
 
-export const Favs = () => <h1>Esto es Favorite</h1>;
+import { useGetFavorites } from "../container/GetFavorites";
+import { ListOfFavs } from "../components/ListOfFavs";
+
+export const Favs = () => {
+  const { data, loading, error } = useGetFavorites();
+
+  if (loading) return "loading...";
+  if (error) return "error";
+  const { favs } = data;
+  return (
+    <>
+      <h1>Favs</h1>
+      <ListOfFavs favs={favs} />
+    </>
+  );
+};
