7379b00f3c0018704e105a910f4e87cfd96eb665
Luis Diaz <Luis>
2023-03-27 15:46:11 -0500

    added navigate to redirect any wrong input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes, Navigate } from "react-router-dom";
 import { AppContext } from "./Context";
 
 import { GlobalStyle } from "./styles/GlobalStyles";
@@ -11,6 +11,7 @@ import { Detail } from "./pages/Detail";
 import { Favs } from "./pages/Favs";
 import { User } from "./pages/User";
 import { NotRegisteredUser } from "./pages/NotRegisteredUser";
+import { NotFound } from "./pages/NotFound";
 
 export const App = () => {
   const { isAuth } = useContext(AppContext);
@@ -25,12 +26,20 @@ export const App = () => {
           <Route path={`/detail/:detailId`} element={<Detail />} />
           <Route
             path={`/favs`}
-            element={isAuth ? <Favs /> : <NotRegisteredUser />}
+            element={isAuth ? <Favs /> : <Navigate replace to="/login" />}
           />
           <Route
             path={`/user`}
-            element={isAuth ? <User /> : <NotRegisteredUser />}
+            element={isAuth ? <User /> : <Navigate replace to="/login" />}
           />
+          <Route
+            exact
+            path="/login"
+            element={
+              !isAuth ? <NotRegisteredUser /> : <Navigate replace to="/" />
+            }
+          />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <NavBar />
       </BrowserRouter>
